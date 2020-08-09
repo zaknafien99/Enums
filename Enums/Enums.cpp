bddfe82e484d27efece036eb27efb9c20c7fe4e5
nick zervos <zaknafien99@hotmail.com>
2020-08-09 18:11:10 +1000

    Created enum type and added case statement

--- Enums/Enums.cpp
+++ Enums/Enums.cpp
@@ -1,8 +1,33 @@
 #include "pch.h"
 
 using namespace System;
+// The Weekday enum definition
+public enum class WeekDay
+{
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+};
 
 int main(array<System::String ^> ^args)
 {
+    // Create a WeekDay
+    WeekDay w = WeekDay::Monday;
+
+    Console::WriteLine("Value of w is {0}", (int)w);
+
+    // Swithc on the weekday
+    switch (w)
+    {
+    case WeekDay::Monday:
+        Console::WriteLine("It's a Monday!");
+        break;
+    case WeekDay::Tuesday:
+        Console::WriteLine("It's a Tuesday!");
+        break;
+    case WeekDay::Wednesday:
+        Console::WriteLine("It's a Wednesday!");
+        break;
+    default:
+        Console::WriteLine("it's some other day....");
+    }
     return 0;
 }
